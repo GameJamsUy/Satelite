4aec76ede3ab878136f9cc72c56fbf187c35c11c
Inti <inti.rodr@gmail.com>
2018-01-27 23:18:32 -0300

    no message

---
+++ Assets/ParalaxDrift.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParalaxDrift : MonoBehaviour {
+    public float speedX;
+    public float speedY;
+    public float minX;
+    public float maxX;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        transform.position = new Vector2(transform.position.x + speedX * Time.deltaTime, transform.position.y);
+        if(transform.position.x <= minX){
+            speedX = -speedX;
+            transform.position = new Vector2(minX + 3, transform.position.y);
+        }
+        if (transform.position.x >= maxX){
+            speedX = -speedX;
+            transform.position = new Vector2(maxX - 3, transform.position.y);
+        }
+    }
+}
